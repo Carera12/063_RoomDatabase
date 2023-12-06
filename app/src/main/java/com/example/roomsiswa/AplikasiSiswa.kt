d77006c1b12a028292beddb96d2e6eb888e7aa3f
Carera12 <silvicarera123@gmail.com>
2023-12-06 18:17:10 +0700

    Add instance container

--- app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/AplikasiSiswa.kt
@@ -1,4 +1,13 @@
 package com.example.roomsiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.roomsiswa.repositori.ContainerApp
+
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+
 }
