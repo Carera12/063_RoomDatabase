733a97c0b4103e94570155f97df662f9e2d7a5d1
Carera12 <silvicarera123@gmail.com>
2023-12-06 18:40:37 +0700

    Add object DestinasiEntry: DestinasiNavigai

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -1,2 +1,11 @@
 package com.example.roomsiswa.ui.halaman
 
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigai
+
+object DestinasiEntry: DestinasiNavigai{
+    override val route: String
+            = "item_entry"
+    override val titleRes: Int
+            = R.string.entry_siswa
+}
