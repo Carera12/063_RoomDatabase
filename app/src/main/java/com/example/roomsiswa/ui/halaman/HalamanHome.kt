55299c9821d562942c6300d484dc94ce44ed319a
Carera12 <silvicarera123@gmail.com>
2023-12-06 19:01:03 +0700

    Add object DestinasiHome : DestinasiNavigai

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.ui.halaman
 
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigai
+
+object DestinasiHome : DestinasiNavigai {
+    override val route: String = "home"
+    override val titleRes: Int = R.string.app_name
+}
