95d0735f4a69c8a5022e61c3e23a0085ff482bde
Carera12 <silvicarera123@gmail.com>
2023-12-06 18:11:55 +0700

    Add data class HomeUiState pada HomeViewModel

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
-class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {}
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
+}
